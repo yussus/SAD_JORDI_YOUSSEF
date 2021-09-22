2d19d34b1f71c7ae6d84a4a64c8489c1fcc323d4
yussus <79423673+yussus@users.noreply.github.com>
2021-09-22 16:03:47 +0200

    Add files via upload

---
+++ EditableBufferedReader.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class EditableBufferedReader extends BufferedReader{
+    Line linia;
+    Console terminal;
+        int posicio, length;
+        static final int ESC = 27;
+        static final int BACKSPACE = 127;
+        static final int RIGHT = 67;
+        static final int LEFT = 68;
+        static final int HOME = 72;
+        static final int END = 70;
+        static final int INSERT = 50;
+        static final int DELETE = 51;
+        static final int CORCHETE = 91;
+        static final int TILDE = 126; //simbol ~
+        static final int INTERROGANT = 63;
+        static final int ENTER = 13;
+
+        static final int SEC_BACKSPACE = 127;
+        static final int ESCAPE_SEC = 2999;
+        static final int SEC_HOME = 3000;
+        static final int SEC_RIGHT = 3001;
+        static final int SEC_LEFT = 3002;
+        static final int SEC_FIN = 3003;
+        static final int SEC_INSERT = 3004;
+        static final int SEC_DELETE = 3005;
+
+    public EditableBufferedReader(Reader in){
+        super(in);
+        linia = new Line();
+    }
+    public setRaw 
